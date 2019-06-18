0952186805a1682492589ceac4658a7d5ffec333
JerryMann <herman.skurko@gmail.com>
2019-06-18 17:47:53 +0300

    Created menu for all dz. Repalced some names in classes

--- app/src/main/java/by/itacademy/pvt/skurkoandroidpvt/dz1/dz1Activity.kt
+++ app/src/main/java/by/itacademy/pvt/skurkoandroidpvt/dz1/dz1Activity.kt
@@ -4,7 +4,7 @@ import android.app.Activity
 import android.os.Bundle
 import by.itacademy.pvt.skurkoandroidpvt.R
 
-class dz1Activity : Activity() {
+class Dz1Activity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dz1)
