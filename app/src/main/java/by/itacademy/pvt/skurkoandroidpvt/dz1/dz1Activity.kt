a16ba1bee7bedea773b9d67f8e49f39273ea1b42
JerryMann <herman.skurko@gmail.com>
2019-06-18 17:21:23 +0300

    Created flags with layouts and colors in activity_dz1

--- app/src/main/java/by/itacademy/pvt/skurkoandroidpvt/dz1/dz1Activity.kt
+++ app/src/main/java/by/itacademy/pvt/skurkoandroidpvt/dz1/dz1Activity.kt
@@ -2,9 +2,12 @@ package by.itacademy.pvt.skurkoandroidpvt.dz1
 
 import android.app.Activity
 import android.os.Bundle
+import by.itacademy.pvt.skurkoandroidpvt.R
 
 class dz1Activity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dz1)
     }
+
 }
