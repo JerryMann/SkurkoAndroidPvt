6ed5e0ebc8178028defa7d6ba1bea1427361522a
JerryMann <herman.skurko@gmail.com>
2019-06-18 17:10:39 +0300

    Started dz1. Created dz1Activity and its layout

---
+++ app/src/main/java/by/itacademy/pvt/skurkoandroidpvt/dz1/dz1Activity.kt
@@ -0,0 +1,10 @@
+package by.itacademy.pvt.skurkoandroidpvt.dz1
+
+import android.app.Activity
+import android.os.Bundle
+
+class dz1Activity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
