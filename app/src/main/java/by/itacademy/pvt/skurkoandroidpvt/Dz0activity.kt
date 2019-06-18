b082d0a3ae117a49316734e0792ab6d216fbd3c8
JerryMann <herman.skurko@gmail.com>
2019-06-18 16:46:10 +0300

    Replaced variables with xml references. Put function swapValues() out of onCreate() function.

--- app/src/main/java/by/itacademy/pvt/skurkoandroidpvt/Dz0activity.kt
+++ app/src/main/java/by/itacademy/pvt/skurkoandroidpvt/Dz0activity.kt
@@ -2,35 +2,13 @@ package by.itacademy.pvt.skurkoandroidpvt
 
 import android.app.Activity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_dz0.*
 
 class Dz0activity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dz0)
-
-        var text1 = R.string.fizz
-        var text2 = R.string.buzz
-        var color1 = resources.getColor(R.color.oneViewColor)
-        var color2 = resources.getColor(R.color.twoViewColor)
-        var button = findViewById<Button>(R.id.button)
-        var textView1 = findViewById<TextView>(R.id.textView1)
-        var textView2 = findViewById<TextView>(R.id.textView2)
-
-        fun swapValues() {
-            var tempText = text1
-            text1 = text2
-            text2 = tempText
-            textView1.setText(text1)
-            textView2.setText(text2)
-            var tempColor = color1
-            color1 = color2
-            color2 = tempColor
-            textView1.setBackgroundColor(color1)
-            textView2.setBackgroundColor(color2)
-        }
 
         button.setOnClickListener {
             swapValues()
@@ -41,5 +19,14 @@ class Dz0activity : Activity() {
         textView2.setOnClickListener {
             swapValues()
         }
+    }
+
+    private fun swapValues() {
+        val tempText = textView1.text
+        textView1.text = textView2.text
+        textView2.text = tempText
+        val tempColor = textView1.background
+        textView1.background = textView2.background
+        textView2.background = tempColor
     }
 }
