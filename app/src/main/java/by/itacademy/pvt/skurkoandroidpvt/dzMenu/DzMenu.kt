d8fceee7ccf3b40782b9e1d6f19d2c69a11787b5
JerryMann <herman.skurko@gmail.com>
2019-06-22 13:51:43 +0300

    fixed some issues form previous dz

--- app/src/main/java/by/itacademy/pvt/skurkoandroidpvt/dzMenu/DzMenu.kt
+++ app/src/main/java/by/itacademy/pvt/skurkoandroidpvt/dzMenu/DzMenu.kt
@@ -3,7 +3,7 @@ package by.itacademy.pvt.skurkoandroidpvt.dzMenu
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
-import by.itacademy.pvt.skurkoandroidpvt.Dz0activity
+import by.itacademy.pvt.skurkoandroidpvt.Dz0Activity
 import by.itacademy.pvt.skurkoandroidpvt.R
 import by.itacademy.pvt.skurkoandroidpvt.dz1.Dz1Activity
 import kotlinx.android.synthetic.main.activity_menu.*
@@ -15,13 +15,11 @@ class DzMenu : Activity() {
         setContentView(R.layout.activity_menu)
 
         button1.setOnClickListener {
-            val intent = Intent(this, Dz0activity::class.java)
-            startActivity(intent)
+            startActivity(Intent(this, Dz0Activity::class.java))
         }
 
         button2.setOnClickListener {
-            val intent = Intent(this, Dz1Activity::class.java)
-            startActivity(intent)
+            startActivity(Intent(this, Dz1Activity::class.java))
         }
     }
 }
