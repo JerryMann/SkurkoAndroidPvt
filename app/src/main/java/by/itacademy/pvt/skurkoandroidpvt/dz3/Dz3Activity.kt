ec47e59af362d098d9af81105e40ac477a2ef162
JerryMann <herman.skurko@gmail.com>
2019-06-23 20:37:53 +0300

    started dz3. Set up activity and its layout to work.

---
+++ app/src/main/java/by/itacademy/pvt/skurkoandroidpvt/dz3/Dz3Activity.kt
@@ -0,0 +1,10 @@
+package by.itacademy.pvt.skurkoandroidpvt.dz3
+
+import android.app.Activity
+import android.os.Bundle
+
+class Dz3Activity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
