afb66153beade6eb657eefd57e12452fe5b10758
JerryMann <herman.skurko@gmail.com>
2019-06-23 21:32:09 +0300

    progress on Dz3Activity, activity_dz3.xml, added styles and dimens

--- app/src/main/java/by/itacademy/pvt/skurkoandroidpvt/dz3/Dz3Activity.kt
+++ app/src/main/java/by/itacademy/pvt/skurkoandroidpvt/dz3/Dz3Activity.kt
@@ -2,9 +2,32 @@ package by.itacademy.pvt.skurkoandroidpvt.dz3
 
 import android.app.Activity
 import android.os.Bundle
+import android.widget.ImageView
+import by.itacademy.pvt.skurkoandroidpvt.R
+import by.itacademy.pvt.skurkoandroidpvt.transform.Circular
+import com.squareup.picasso.Callback
+import com.squareup.picasso.Picasso
+import java.lang.Exception
 
 class Dz3Activity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dz3)
+
+        val imageView = findViewById<ImageView>(R.id.image)
+        val url = R.string.url_sample
+
+        Picasso.get()
+            .load(url.toString())
+            .transform(Circular())
+            .into(imageView, object : Callback {
+                override fun onSuccess() {
+                    return
+                }
+
+                override fun onError(e: Exception?) {
+                    imageView.background = getDrawable(R.drawable.error_image_icon_23)
+                }
+            })
     }
 }
