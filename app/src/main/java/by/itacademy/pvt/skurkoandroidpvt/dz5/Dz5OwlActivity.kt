b1641173a9edd7ec82614fbce761fd8fe5f9b22f
JerryMann <herman.skurko@gmail.com>
2019-06-29 12:29:43 +0300

    Finished owl animation

--- app/src/main/java/by/itacademy/pvt/skurkoandroidpvt/dz5/Dz5OwlActivity.kt
+++ app/src/main/java/by/itacademy/pvt/skurkoandroidpvt/dz5/Dz5OwlActivity.kt
@@ -1,12 +1,28 @@
 package by.itacademy.pvt.skurkoandroidpvt.dz5
 
 import android.app.Activity
+import android.graphics.drawable.AnimationDrawable
 import android.os.Bundle
+import android.widget.ImageView
 import by.itacademy.pvt.skurkoandroidpvt.R
 
-class Dz5OwlActivity: Activity() {
+class Dz5OwlActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dz5_owl)
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        val owlAnimation = findViewById<ImageView>(R.id.dz5Owl)
+        (owlAnimation.background as AnimationDrawable).start()
+    }
+
+    override fun onPause() {
+        super.onPause()
+
+        val owlAnimation = findViewById<ImageView>(R.id.dz5Owl)
+        (owlAnimation.background as AnimationDrawable).stop()
     }
 }
