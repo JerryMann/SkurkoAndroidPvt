7a0d5eb27870092ac07e76054d50cce43af5b14d
JerryMann <herman.skurko@gmail.com>
2019-07-18 15:08:09 +0300

    added fragment with bottomSheetBehavior

--- app/src/main/java/by/itacademy/pvt/skurkoandroidpvt/dz9/Dz9Activity.kt
+++ app/src/main/java/by/itacademy/pvt/skurkoandroidpvt/dz9/Dz9Activity.kt
@@ -2,14 +2,17 @@ package by.itacademy.pvt.skurkoandroidpvt.dz9
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.FrameLayout
 import by.itacademy.pvt.skurkoandroidpvt.R
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.material.bottomsheet.BottomSheetBehavior
 
 class Dz9Activity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
+    private lateinit var bottomSheetBehavior: BottomSheetBehavior<FrameLayout>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,6 +21,9 @@ class Dz9Activity : AppCompatActivity(), OnMapReadyCallback {
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+
+        bottomSheetBehavior = BottomSheetBehavior.from(findViewById(R.id.dz9Container1))
+
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
